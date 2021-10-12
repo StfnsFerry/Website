efe9129b50ab998af9b51274b803f53fa28afcc0
Stefanus Ferry Susanto <91201205+StefanusFerry11@users.noreply.github.com>
2021-10-12 15:16:03 +0700

    Add files via upload

---
+++ Belajar Buat Website.php
@@ -0,0 +1,71 @@
+<!doctype html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Belajar Bikin Website</title>
+    <link rel="stylesheet" href="CSS.css">
+  </head>
+  <body>
+    <div class="header">
+      <div class="header-logo">Learning</div>
+      <div class="header-list">
+        <ul>
+          <li>Tentang</li>
+          <li>Pelajaran</li>
+          <li>Hubungi Kami</li>
+        </ul>
+      </div>
+    </div>
+
+    <div class="main">
+      <div class="copy-container">
+        <h1>HELLO WORLD<span>.</span></h1>
+        <h2>Ayo belajar coding</h2>
+      </div>
+      
+      <div class="contents">
+        <h3 class="section-title">Pelajaran</h3>
+        <div class="contents-item">
+          <img src="https://s3-ap-northeast-1.amazonaws.com/progate/shared/images/lesson/html/study/php.svg">
+          <p>HTML & CSS</p>
+        </div>
+        <div class="contents-item">
+          <img src="https://s3-ap-northeast-1.amazonaws.com/progate/shared/images/lesson/html/study/php.svg">
+          <p>PHP</p>
+        </div>
+        <div class="contents-item">
+          <img src="https://s3-ap-northeast-1.amazonaws.com/progate/shared/images/lesson/html/study/ruby.svg">
+          <p>Ruby</p>
+        </div>
+        <div class="contents-item">
+          <img src="https://s3-ap-northeast-1.amazonaws.com/progate/shared/images/lesson/html/study/swift.svg">
+          <p>Swift</p>
+        </div>
+      </div>
+      
+      <div class="contact-form">
+        <h3 class="section-title">Hubungi kami</h3>
+        <p>Email (Wajib Diisi)</p>       
+            <input>
+        
+        <p>Pesan (Wajib Diisi)</p>
+            <textarea></textarea>
+        
+        <p>*Bidang Wajib Diisi</p>
+            <input class="contact-submit" type= "submit" value= Kirim>
+        
+      </div>
+    </div>
+
+    <div class="footer">
+      <div class="footer-logo">Learning</div>
+      <div class="footer-list">
+        <ul>
+          <li>Tentang</li>
+          <li>Karier</li>
+          <li>Hubungi Kami</li>
+        </ul>
+      </div>
+    </div>
+  </body>
+</html>
